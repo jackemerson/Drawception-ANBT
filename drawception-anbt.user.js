27891113ceea8382900dc011b7d7a793a9fe3ec3
EnderDragonneau <30511303+EnderDragonneau@users.noreply.github.com>
2018-12-28 20:30:15 +0200

    1.147.2018.12

--- drawception-anbt.user.js
+++ drawception-anbt.user.js
@@ -2,7 +2,7 @@
 // @name         Drawception ANBT
 // @author       Grom PE
 // @namespace    http://grompe.org.ru/
-// @version      1.146.2018.12
+// @version      1.147.2018.12
 // @description  Enhancement script for Drawception.com - Artists Need Better Tools
 // @downloadURL  https://raw.github.com/EnderDragonneau/Drawception-ANBT/master/drawception-anbt.user.js
 // @match        http://drawception.com/*
@@ -12,8 +12,8 @@
 // @license      Public domain
 // ==/UserScript==
 const wrapped = () => {
-  const SCRIPT_VERSION = '1.146.2018.12'
-  const NEWCANVAS_VERSION = 47 // Increase to update the cached canvas
+  const SCRIPT_VERSION = '1.147.2018.12'
+  const NEWCANVAS_VERSION = 48 // Increase to update the cached canvas
   const SITE_VERSION = 'c75fc938' // Last seen site version
 
   // == DEFAULT OPTIONS ==
