b20fbc27c3842e3b0c6fb38bd79f88a0740e9778
EnderDragonneau <30511303+EnderDragonneau@users.noreply.github.com>
2019-04-28 13:28:48 +0200

    Add support for the new palette: Easter

--- drawception-anbt.user.js
+++ drawception-anbt.user.js
@@ -477,7 +477,8 @@ const wrapped = () => {
         theme_sin_city: ['Sin City', '#000000'],
         theme_lucky_clover: ['Lucky Clover', '#0c442c'],
         theme_drawception: ["D's Exclusive", '#0ee446'],
-        theme_retina_burn: ['Retina Burn', '#ff0b11']
+        theme_retina_burn: ['Retina Burn', '#ff0b11'],
+        theme_easter: ['Easter', '#ddf7a8']
       }
       const pal = info.palette
       let paldata
