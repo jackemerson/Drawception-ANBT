e94390ea05dd2c531fa59486379cf38eb6262b63
EnderDragonneau <30511303+EnderDragonneau@users.noreply.github.com>
2019-07-30 16:26:50 +0200

    Fix addch.py

--- addch.py
+++ addch.py
@@ -2,7 +2,7 @@ import re,subprocess
 
 gitcommand = 'git log origin..master --format="- %s"'
 
-news = subprocess.check_output(gitcommand)
+news = subprocess.check_output(gitcommand, shell=True).decode('utf-8')
 if len(news)<2:
   print("Nothing new.")
   exit()
@@ -11,7 +11,7 @@ f = open("CHANGELOG.txt", "rb")
 changelog = f.read()
 f.close()
 
-ver = re.match("(\d+)\.(\d+)\.(\d+)\.(\d+)", changelog)
+ver = re.match("(\d+)\.(\d+)\.(\d+)\.(\d+)", changelog.decode('utf-8'))
 l = list(ver.group(1, 2, 3, 4))
 l[1] = int(l[1], 10) + 1
 newver = "%s.%s.%s.%s" % tuple(l)
@@ -19,7 +19,7 @@ newver = "%s.%s.%s.%s" % tuple(l)
 additions = newver + "\n" + news + "\n"
 
 f = open("CHANGELOG.txt", "wb")
-f.write(additions)
+f.write(str.encode(additions))
 f.write(changelog)
 f.close()
 
