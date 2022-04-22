bcce29d3da499701a7ec478147958269fb535227
Jack Emerson <mail@jackemerson.me>
2022-04-22 11:42:38 +1000

    Further testing for release of one key.

--- src/newcanvas/js/functions/bindEvents/svgContainerEvents/mouseUp.js
+++ src/newcanvas/js/functions/bindEvents/svgContainerEvents/mouseUp.js
@@ -7,22 +7,24 @@ import { windowMouseMove } from '../windowEvents/mouseMove'
 export function mouseUp(event) {
   const { options } = window
 
-  //TODO: add check to see if other button is active
-  if (event.button === 0 || event.button === 2) {
-
-    
-
+  // add check to see if other drawing button is active
+  
+  
+  if (event.button === 0 || event.button === 2) { // if we just released one of these
     event.preventDefault()
     if (anbt.isStroking) strokeEnd()
 
-    if (event.buttons & 3) { // bitwise check for left or right mouse button still in use
+    if (event.buttons & 3) { // bitwise check for left or right mouse button still active
       const lastPoint = anbt.points[anbt.points.length - 1];
-      let button = (event.button === 0); // true if left button
-      strokeBegin(lastPoint.x, lastPoint.y, button);
-    } else {
+      const leftUp = (event.button === 0); // true if left button was released
+
+      strokeBegin(lastPoint.x, lastPoint.y, !leftUp);
+      
+      console.log(`Released: ${event.button ? 'right' : 'left'}, Still pressed: ${event.buttons}`);
+    } else { // no longer drawing
       if (options.hideCross) { ID('svgContainer').classList.remove('hidecursor'); }
       window.removeEventListener('mouseup', mouseUp)
       window.removeEventListener('mousemove', windowMouseMove)
-    }
+   }
   }
 }
